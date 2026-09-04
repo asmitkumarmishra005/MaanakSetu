769bb4e499d0c525d2c270257b063f5043a19025
Asmit <asmitkumarmishra005@gmail.com>
2026-09-04 22:33:40 +0530

    Fix local frontend CORS

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,9 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://localhost:5174",
   "http://127.0.0.1:5173",
+  "http://127.0.0.1:5174",
   "https://asmitkumarmishra005.github.io",
 ];
 
